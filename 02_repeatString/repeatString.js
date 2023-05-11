d4b81d69d33a3b64f9b97ed479a4a102b9a44f48
OrangeJooz <kyernys@gmail.com>
2023-05-10 22:19:28 -0600

    Create repeatString function and error proof

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, numRepeat) {
+    let oldStr = str;
+    if (numRepeat > 0)
+        for (let i = 1; i < numRepeat; i++){
+            str += '' + oldStr;
+        }
+    else if (numRepeat === 0)
+        return '';
+    else if (numRepeat < 0)
+        return 'ERROR';
+    return str;
 };
 
 // Do not edit below this line
