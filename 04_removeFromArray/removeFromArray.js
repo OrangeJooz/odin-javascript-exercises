09cb9cb624736e758e39604a968a5da02e9a1f79
OrangeJooz <kyernys@gmail.com>
2023-05-10 23:51:49 -0600

    Create main function with error handling

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    for (let i = 1; i < arguments.length; i++){
+        let index = array.indexOf(arguments[i])
 
+        //check num exists in array
+        if (index >= 0)
+            array.splice(index, 1);
+    }
+    console.log(array);
+    return array;
 };
 
 // Do not edit below this line
