9e9793857099a9bee0867ecd4d8977aa200756e1
OrangeJooz <kyernys@gmail.com>
2023-05-11 00:34:40 -0600

    Create main function with error handling

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    let large = getLargest(a,b);
+    let small = getSmallest(a,b);
+    
+    //check if real number greater than 0
+    if (typeof a === 'number' && typeof b === 'number' && a > 0 && b > 0){
+        while (small++ < large){
+            sum+=small;
+        }
+        return (sum + 1);
+    }
+    else
+        return 'ERROR';
 };
+
+function getLargest(a,b){
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+function getSmallest(a,b){
+    if (a < b)
+        return a;
+    else
+        return b;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
