11de449b0d325d14700c1184e7b8e23df4c0816a
OrangeJooz <kyernys@gmail.com>
2023-05-11 02:50:30 -0600

    Create main function with error handling

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,37 @@
 const palindromes = function () {
-
+    let success = false;
+    Array.from(arguments).forEach((element)=>{
+        let word = cleanString(element);
+        success = isPalindrome(word, 0);
+    })
+    return success;
 };
+
+function cleanString(str){
+    let newStr = '';
+    let array = str.split('');
+    array.forEach(element => {
+        if (isLetter(element))
+            newStr += element.toLowerCase();
+    });
+    return newStr;
+}
+
+function isLetter(char){
+    if (char.match(/[a-z]/i) === null)
+        return false;
+    else
+        return true; 
+}
+
+function isPalindrome(str, index){
+    if (str[index] != str[str.length - 1 - index])
+        return false;
+    else if (index === str.length - 1)
+        return true;
+    else
+        return isPalindrome(str, index + 1);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
