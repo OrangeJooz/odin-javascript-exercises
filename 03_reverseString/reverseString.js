8cb33a4f09235b049c29693f65933dd1e524ab81
OrangeJooz <kyernys@gmail.com>
2023-05-10 22:35:14 -0600

    Create reverseString function and error handling

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStr = '';
+    let array = str.split('');
+    for (let i = array.length - 1; i >= 0; i--){
+        newStr += array[i];
+    }
+    return newStr;
 };
 
 // Do not edit below this line
