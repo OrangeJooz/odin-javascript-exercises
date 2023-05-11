a54027f7a1a275369a5a5b4177f6c8e9fd3d1bec
OrangeJooz <kyernys@gmail.com>
2023-05-11 02:02:17 -0600

    Create math functions for add, subtract, multiply, sum, exponent, and factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  array.forEach(element => {
+    if (typeof element === 'number')
+      sum+=element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  array.forEach(element => {
+    total *= element;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let total = 1;
+  for (let i = 0; i < b; i++){
+    total *= a;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  for (let i = a; i > 0; i--){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
